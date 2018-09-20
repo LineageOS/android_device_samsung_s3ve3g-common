6d9439bb7d20f4a59029601f42c40a2e7a5200ad
Kevin F. Haggerty <haggertk@lineageos.org>
2018-09-20 08:19:36 -0600

    s3ve3g-common: Set SDK API level for required executables
    * This singularly handles both text relocation (M) and mutex (P)
      linker restrictions
    * mediaserver - Needs text relocations
    * mm-qcamera-daemon - Needs both text relocations and mutex restrictions
      relaxed
    
    Change-Id: I97a79fb57aaadf5cf62894489522b2de6d8c2e36

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -61,6 +61,9 @@ TARGET_KERNEL_SOURCE := kernel/samsung/msm8226
 
 # Legacy BLOB Support
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
+TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
+    /system/bin/mediaserver=22 \
+    /system/vendor/bin/mm-qcamera-daemon=22
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
