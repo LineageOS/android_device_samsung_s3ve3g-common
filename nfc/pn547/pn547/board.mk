a30c136edf54880e6e493b58691878eb675ce402
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-09-09 15:49:47 +0200

    s3ve3g-common: update NFC for pie

--- nfc/pn547/pn547/board.mk
+++ nfc/pn547/pn547/board.mk
@@ -19,4 +19,3 @@ include device/samsung/s3ve3g/nfc/board.mk
 
 # PN547 specific flags
 BOARD_NFC_CHIPSET := pn547
-BOARD_NFC_DEVICE := "/dev/pn547"
--- nfc/pn547/pn547/product.mk
+++ nfc/pn547/pn547/product.mk
@@ -18,16 +18,9 @@
 $(call inherit-product, device/samsung/s3ve3g/nfc/product.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
     libpn547_fw \
-    libpn547_fw_pku \
-    libpn547_fw_platform \
     nfc_nci.pn54x.default
 
 PRODUCT_COPY_FILES += \
     device/samsung/s3ve3g/nfc/pn547/libnfc-nci.conf:system/etc/libnfc-nci.conf \
     device/samsung/s3ve3g/nfc/pn547/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.nfc_nci=pn54x \
-    ro.nfc.port=I2C
