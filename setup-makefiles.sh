388afbb6e4f390a8721d6b61e8dea97e12bdf0bb
Michael Bestas <mkbestas@lineageos.org>
2021-02-08 05:01:48 -0700

    s3ve3g-common: Switch to standalone extract utils
    * Align with templates while we are at it
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,62 +1,45 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-export INITIAL_COPYRIGHT_YEAR=2014
+if [ -z "${DEVICE_COMMON}" ]; then
+    echo ""
+    echo "error: This is a script in a common tree. Please execute" $(basename $0) "from a device tree."
+    echo ""
+    exit 1
+fi
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "${HELPER}"
+source "${HELPER}"
 
-# Initialize the helper for common device
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-# Copyright headers and common guards
+# Warning headers and guards
 write_headers "s3ve3gxx s3ve3gjv s3ve3gds"
 
-write_makefiles "${MY_DIR}/common-proprietary-files.txt"
+# The standard common blobs
+write_makefiles "${MY_DIR}/common-proprietary-files.txt" true
 
+# Finish
 write_footers
 
-if [ ! -z "${VARIANT_COPYRIGHT_YEAR}" ]; then
-    export INITIAL_COPYRIGHT_YEAR="${VARIANT_COPYRIGHT_YEAR}"
-fi
+export BOARD_COMMON=msm8226-common
 
-# Reinitialize the helper for device
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
-
-# Copyright headers and guards
-write_headers
-
-for BLOB_LIST in "${MY_DIR}/../${DEVICE}/device-proprietary-files*.txt"; do
-    write_makefiles ${BLOB_LIST}
-done
-
-write_footers
-
-./../msm8226-common/setup-makefiles.sh $@
+"./../../${VENDOR}/${BOARD_COMMON}/setup-makefiles.sh" "$@"
